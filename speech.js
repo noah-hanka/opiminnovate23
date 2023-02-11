b2e635c0bc3ef4fcf49cc2f0e5911818a6d83257
Noah <hankanz@outlook.com>
2023-02-11 15:25:40 -0500

    voice recog

---
+++ speech.js
@@ -0,0 +1,26 @@
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const SpeechGrammarList = window.SpeechGrammarList || webkitSpeechGrammarList;
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+
+const vocab = ["elevate", "floor", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
+
+const grammar = `#JSGF V1.0; grammar vocab; public <vocab> = ${vocab.join(' | ')};`
+
+const recognition = new SpeechRecognition();
+const speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+
+recognition.grammars = speechRecognitionList;
+recognition.continuous = true;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+recognition.start();
+
+
+recognition.onresult = (event) => {
+    const command = event.results[event.results.length - 1][0].transcript;
+    console.log(command);
+}
